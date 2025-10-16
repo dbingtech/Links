52e778a999425b2d6bf3ce6f71172d5a2fa45280
Dan Bingman <techfromtheteacher@gmail.com>
2025-10-16 15:37:42 -0400

    Added Thunderbird

--- js/navbar.js
+++ js/navbar.js
@@ -7,9 +7,10 @@ class NavBar extends HTMLElement {
         <div class="topnav">
             
             <a href="https://www.bdilinks.bingman.xyz">BDI Links</a>
-            <a href="more_personal.html">Computer Setup</a>
+            <a href="computer_setup.html">Computer Setup</a>
             <a href="mentor.html">Mentor</a>
             <a href="https://houseofmewsrescue.org">HOMR</a>
+            <a href="personal.html">Misc</a>
         </div> 
         `;
     }
